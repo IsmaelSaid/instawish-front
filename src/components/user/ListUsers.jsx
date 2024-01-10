f6f9a96de97dda649c4e1c6a8bb3c6515f62e0c4
SAID <ismael.saidpro@protonmail.com>
2024-01-10 05:13:42 +0100

    Add : Function to list all registered users.

--- src/components/user/ListUsers.jsx
+++ src/components/user/ListUsers.jsx
@@ -1,31 +1,60 @@
+import { useEffect, useState } from "react";
 
 function ListUsers() {
+    const [users, setUsers] = useState([]);
 
-    var header = new Headers()
-    header.append('Authorization', `Bearer ${localStorage.getItem('token')}`)
-    
+    useEffect(() => {
+        const header = new Headers();
+        header.append('Authorization', `Bearer ${localStorage.getItem('token')}`);
 
-    var requestOptions = {
-        method: 'GET',
-        headers: header,
-    };
-    fetch('https://symfony-instawish.formaterz.fr/api/users', requestOptions).then((response) => {
-        if (!response.ok) {
-            throw new Error(`HTTP error: ${response.status}`);
-        }
-        return response.json()
-    }).then(
-        (value) => {
-            console.log(value)
-            // userData.push(value) ?
-            // useState ?
-        }
-    ).catch((reason) => {
-        console.log('erreur')
-    })
+        const requestOptions = {
+            method: 'GET',
+            headers: header,
+        };
 
-    // Afficher la liste des utilisateurs dans le dom avec l'option de follow.
-    return <ul></ul>
+        fetch('https://symfony-instawish.formaterz.fr/api/users', requestOptions)
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error(`HTTP error: ${response.status}`);
+                }
+                return response.json();
+            })
+            .then((value) => {
+                setUsers(value);
+            })
+            .catch((reason) => {
+                console.log('erreur', reason);
+            });
+    }, []);
 
+    return (
+        <div>
+            {Object.entries(users).length > 0 ? (
+                <table>
+                    <thead>
+                        <tr>
+                            <th>username</th>
+                            <th>email</th>
+                            <th>id</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {Object.entries(users).map((value) => {
+                            const [index, { id, email, username }] = value;
+                            return (
+                                <tr key={id}>
+                                    <td>{username}</td>
+                                    <td>{email}</td>
+                                    <td>{id}</td>
+                                </tr>
+                            );
+                        })}
+                    </tbody>
+                </table>
+            ) : (
+                <p>Aucune donnée à afficher</p>
+            )}
+        </div>
+    );
 }
 export default ListUsers
