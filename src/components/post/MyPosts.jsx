cfe9033d95ed174afa8aef34b845a09235bf0609
SAID <ismael.saidpro@protonmail.com>
2024-01-10 10:11:15 +0100

    Update : The user can remove his posts.

--- src/components/post/MyPosts.jsx
+++ src/components/post/MyPosts.jsx
@@ -1,8 +1,43 @@
 import { useEffect, useState } from "react"
-import { CardBody, Container, Button, Row } from "react-bootstrap";
+import { CardBody, Container, Button } from "react-bootstrap";
 import Card from 'react-bootstrap/Card';
 
 function MyPosts() {
+    const handleDelete = (e) => {
+        // console.log("demande de suppression")
+        const header = new Headers()
+        header.append('Authorization', `Bearer ${localStorage.getItem('token')}`);
+
+        const requestOptions = {
+            method: 'POST',
+            headers: header,
+        };
+
+        const target = e.target.value
+        fetch(`https://symfony-instawish.formaterz.fr/api/post/remove/${target}`, requestOptions).then((response) => {
+            if (!response.ok) {
+                /**
+                 * 
+                 */
+                throw new Error(`HTTP error: ${response.status}`);
+
+            }
+            return response.ok
+        }).then((value) => {
+            /**
+             * Pas d'effets...
+             */
+            
+            console.log(`Suppression de ${target} réussit`);
+            // Déclenchez set post pour activer une maj du dom
+            setPosts(posts.filter((post) => post.id != target))
+        }).catch((reason) => {
+            /**
+             * Traitement des erreurs
+            */
+            console.log(`Suppression de ${target} échouée`);
+        })
+    }
     const id = localStorage.getItem('id')
     const [posts, setPosts] = useState([])
     useEffect(() => {
@@ -30,25 +65,22 @@ function MyPosts() {
     }, [])
     return (
         <Container>
-            <div>
+            {posts.length > 0 ? (<div>
                 {Object.entries(posts).map((value, index) => {
-                    const [, { imageUrl, description }] = value
+                    const [, { id, imageUrl, description }] = value
                     return (
                         <Card key={index} className="mt-4">
                             <CardBody>
                                 <Card.Img variant="top" src={`https://symfony-instawish.formaterz.fr/${imageUrl}`} />
                                 <Card.Text>{description}</Card.Text>
                                 <div className="d-flex">
-                                    <Button variant="primary" className="mx-2">Supprimer</Button>
+                                    <Button value={id} variant="primary" className="mx-2" onClick={handleDelete}>Supprimer</Button>
                                 </div>
                             </CardBody>
                         </Card>)
                 })}
-            </div>
-
+            </div>) : <p>Aucun post</p>}
         </Container>
     )
-
 }
-
 export default MyPosts
